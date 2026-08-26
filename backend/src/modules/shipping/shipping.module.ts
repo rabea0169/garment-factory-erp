208890bd71ad13c3ee804d3cbae785bef6b8ee49
rabea0169 <1.2429624e+07+rabea0169@users.noreply.github.com>
2026-08-26 05:14:26 +0000

    fix(gf-0019): import inventory module for shipping

--- backend/src/modules/shipping/shipping.module.ts
+++ backend/src/modules/shipping/shipping.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ShippingController } from './shipping.controller';
 import { ShippingService } from './shipping.service';
+import { InventoryModule } from '../inventory/inventory.module';
 
 @Module({
+  imports: [InventoryModule],
   controllers: [ShippingController],
   providers: [ShippingService],
 })
